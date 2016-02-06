1b554a5f1a9599a453c43ad1b8989ec5ec128012
shadowfax6894 <anniekelly6894@gmail.com>
2016-02-06 16:27:58 -0700

    unit testing

--- node_modules/terriajs/node_modules/html2canvas/tests/node/package.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-var html2canvas = require('../../');
-
-describe("Package", function() {
-    it("should have html2canvas defined", function() {
-        assert.equal(typeof(html2canvas), "function");
-    });
-});
