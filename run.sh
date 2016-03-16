fddb50c81d9fccaf93c8de42b70eba8bfd44c5a1
shadowfax6894 <anniekelly6894@gmail.com>
2016-03-16 17:12:20 -0600

    added backup BACKUP db file

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #Dump data from mysql before killing all of the containers
 #Helps keep data persistent
+
+#If you get an error "cryodb database not found", comment these next two lines.
+#Run the script, uncomment these lines, run "cat db_backup.sql > static/Data/cryodo.sql"
+#and run the script again
 docker exec -it db sudo sh -c "mysqldump cryodb > /var/lib/mysql/cryodb.sql"
 docker cp db:/var/lib/mysql/cryodb.sql ./static/Data/cryodb.sql
 
