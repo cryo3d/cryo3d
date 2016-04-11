15c22ddb48cab94b6a72bd09eb6c7b6c13b4938b
shadowfax6894 <anniekelly6894@gmail.com>
2016-04-11 15:56:15 -0600

    added layer picker modal

--- static/Javascript/layers.js
+++ static/Javascript/layers.js
@@ -1,83 +1,78 @@
-function initLayers() {
-    var layers = viewer.scene.imageryLayers;
+var layers = viewer.scene.imageryLayers;
 /*
-    var correctedReflectance = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
-        url: "//map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2016-03-27&layer=VIIRS_SNPP_CorrectedReflectance_TrueColor",
-        layer: "MODIS_Terra_CorrectedReflectance_TrueColor",
-        style: "",
-        format: "image/jpeg",
-        tileMatrixSetID: "EPSG4326_250m",
-        maximumLevel: 8,
-        tileWidth: 256,
-        tileHeight: 256,
-        tilingScheme: gibs.GeographicTilingScheme()
-    }));
+var correctedReflectance = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
+    url: "//map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2016-03-27&layer=VIIRS_SNPP_CorrectedReflectance_TrueColor",
+    layer: "MODIS_Terra_CorrectedReflectance_TrueColor",
+    style: "",
+    format: "image/jpeg",
+    tileMatrixSetID: "EPSG4326_250m",
+    maximumLevel: 8,
+    tileWidth: 256,
+    tileHeight: 256,
+    tilingScheme: gibs.GeographicTilingScheme()
+}));
 
-    correctedReflectance.alpha = 1.0;    // 0.0 is transparent, 1.0 is opaque
+correctedReflectance.alpha = 1.0;    // 0.0 is transparent, 1.0 is opaque
 */
 
-    var terraSeaIce = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
-        url: "https://map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2016-03-27&layer=MODIS_Terra_Sea_Ice",
-        layer: "MODIS_Terra_Sea_Ice",
-        style: "",
-        format: "image/png",
-        tileMatrixSetID: "EPSG4326_1km",
-        maximumLevel: 8,
-        tileWidth: 256,
-        tileHeight: 256,
-        tilingScheme: gibs.GeographicTilingScheme()
-    }));
+var terraSeaIce = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
+    url: "https://map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2016-03-27&layer=MODIS_Terra_Sea_Ice",
+    layer: "MODIS_Terra_Sea_Ice",
+    style: "",
+    format: "image/png",
+    tileMatrixSetID: "EPSG4326_1km",
+    maximumLevel: 8,
+    tileWidth: 256,
+    tileHeight: 256,
+    tilingScheme: gibs.GeographicTilingScheme()
+}));
 
-    terraSeaIce.alpha = 1.0;
+terraSeaIce.alpha = 1.0;
 
-    var aquaSeaIce = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
-        url: "//map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2016-03-27&layer=MODIS_Aqua_Sea_Ice",
-        layer: "MODIS_Aqua_Sea_Ice",
-        style: "",
-        format: "image/png",
-        tileMatrixSetID: "EPSG4326_1km",
-        maximumLevel: 8,
-        tileWidth: 256,
-        tileHeight: 256,
-        tilingScheme: gibs.GeographicTilingScheme()
-    }));
+var aquaSeaIce = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
+    url: "//map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2016-03-27&layer=MODIS_Aqua_Sea_Ice",
+    layer: "MODIS_Aqua_Sea_Ice",
+    style: "",
+    format: "image/png",
+    tileMatrixSetID: "EPSG4326_1km",
+    maximumLevel: 8,
+    tileWidth: 256,
+    tileHeight: 256,
+    tilingScheme: gibs.GeographicTilingScheme()
+}));
 
-    aquaSeaIce.alpha = 1.0;
+aquaSeaIce.alpha = 1.0;
 
 /*
-    var blackMarble = layers.addImageryProvider(new Cesium.TileMapServiceImageryProvider({
-        url : '//cesiumjs.org/tilesets/imagery/blackmarble',
-        maximumLevel : 8,
-        credit : 'Black Marble imagery courtesy NASA Earth Observatory'
-    }));
+var blackMarble = layers.addImageryProvider(new Cesium.TileMapServiceImageryProvider({
+    url : '//cesiumjs.org/tilesets/imagery/blackmarble',
+    maximumLevel : 8,
+    credit : 'Black Marble imagery courtesy NASA Earth Observatory'
+}));
 
-    blackMarble.alpha = 0.5; // 0.0 is transparent.  1.0 is opaque.
+blackMarble.alpha = 0.5; // 0.0 is transparent.  1.0 is opaque.
 */
 
-    var coastlines = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
-        url: "//map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?l=Reference_Features",
-        layer: "Reference_Features",
-        style: "",
-        format: "image/png",
-        tileMatrixSetID: "EPSG4326_250m",
-        maximumLevel: 8,
-        tileWidth: 256,
-        tileHeight: 256,
-        tilingScheme: gibs.GeographicTilingScheme()
-    }));
+var coastlines = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
+    url: "//map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?l=Reference_Features",
+    layer: "Reference_Features",
+    style: "",
+    format: "image/png",
+    tileMatrixSetID: "EPSG4326_250m",
+    maximumLevel: 8,
+    tileWidth: 256,
+    tileHeight: 256,
+    tilingScheme: gibs.GeographicTilingScheme()
+}));
 
-    coastlines.alpha = 1.0;
+coastlines.alpha = 1.0;
 
-    // Add terrian data source
-    var terrainProvider = new Cesium.CesiumTerrainProvider({
-        url: "//assets.agi.com/stk-terrain/world",
-        requestVertexNormals: true
-    });
-    viewer.scene.terrainProvider = terrainProvider;
+// Add terrian data source
+var terrainProvider = new Cesium.CesiumTerrainProvider({
+    url: "//assets.agi.com/stk-terrain/world",
+    requestVertexNormals: true
+});
+viewer.scene.terrainProvider = terrainProvider;
 
-    // Add lighting effects
-    //viewer.scene.globe.enableLighting = true;
-
-}
-
-initLayers();
+// Add lighting effects
+//viewer.scene.globe.enableLighting = true;
