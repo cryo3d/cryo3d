7092ff461839481d32360c147bd4ffba5937766d
liveshadow <oliviaabrant@rgnt1-62-74-dhcp.int.colorado.edu>
2016-04-25 18:58:12 -0600

    cleaned up layers stuff

--- static/Javascript/layerPickerModal.js
+++ static/Javascript/layerPickerModal.js
@@ -25,18 +25,13 @@ function addLayerToggleOption(label, layerName, minValue, maxValue, stepValue, d
 
 }
 
-addLayerToggleOption("Terra Sea Ice", "terraSeaIce", 0.0, 1.0, 0.1, 1.0);
-addLayerToggleOption("Aqua Sea Ice", "aquaSeaIce", 0.0, 1.0, 0.1, 1.0);
-addLayerToggleOption("Terra Snow Cover", "terraSnowCover", 0.0, 1.0, 0.1, 1.0);
-addLayerToggleOption("Aqua Snow Cover", "aquaSnowCover", 0.0, 1.0, 0.1, 1.0);
+addLayerToggleOption("Sea Ice", "aquaSeaIce", 0.0, 1.0, 0.1, 1.0);
+addLayerToggleOption("Snow Cover", "terraSnowCover", 0.0, 1.0, 0.1, 1.0);
 addLayerToggleOption("Soil Temperature", "soilTemperature", 0.0, 1.0, 0.1, 1.0);
 addLayerToggleOption("Soil Moisture", "soilMoisture", 0.0, 1.0, 0.1, 0.5);
 addLayerToggleOption("Coastlines", "coastlines", 0.0, 1.0, 0.1, 1.0);
-
-var terraSeaIceSlider = document.getElementById("terraSeaIce");
-terraSeaIceSlider.onmouseup = function() {
-	updateLayerAlpha(terraSeaIce, terraSeaIceSlider.value);
-};
+addLayerToggleOption("Earth at Night", "night", 0.0, 1.0, 0.1, 0.0);
+addLayerToggleOption("Satellite Imagery", "correctedReflectance", 0.0, 1.0, 0.1, 0.0);
 
 var aquaSeaIceSlider = document.getElementById("aquaSeaIce");
 aquaSeaIceSlider.onmouseup = function() {
@@ -46,11 +41,6 @@ aquaSeaIceSlider.onmouseup = function() {
 var terraSnowCoverSlider = document.getElementById("terraSnowCover");
 terraSnowCoverSlider.onmouseup = function() {
 	updateLayerAlpha(terraSnowCover, terraSnowCoverSlider.value);
-};
-
-var aquaSnowCoverSlider = document.getElementById("aquaSnowCover");
-aquaSnowCoverSlider.onmouseup = function() {
-	updateLayerAlpha(aquaSnowCover, aquaSnowCoverSlider.value);
 };
 
 var soilTemperatureSlider = document.getElementById("soilTemperature");
@@ -66,6 +56,16 @@ soilMoistureSlider.onmouseup = function() {
 var coastlinesSlider = document.getElementById("coastlines");
 coastlinesSlider.onmouseup = function() {
 	updateLayerAlpha(coastlines, coastlinesSlider.value);
+};
+
+var nightSlider = document.getElementById("night");
+nightSlider.onmouseup = function() {
+	updateLayerAlpha(night, nightSlider.value);
+};
+
+var correctedReflectanceSlider = document.getElementById("correctedReflectance");
+correctedReflectanceSlider.onmouseup = function() {
+	updateLayerAlpha(correctedReflectance, correctedReflectanceSlider.value);
 };
 
 var e3 = document.getElementById('layerPicker');
--- static/Javascript/layers.js
+++ static/Javascript/layers.js
@@ -1,7 +1,6 @@
 var layers = viewer.scene.imageryLayers;
 //layers stacked on globe in order, top-down, so put base layers first
 
-/*
 var correctedReflectance = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
     url: "//map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2016-03-27&layer=VIIRS_SNPP_CorrectedReflectance_TrueColor",
     layer: "MODIS_Terra_CorrectedReflectance_TrueColor",
@@ -14,10 +13,8 @@ var correctedReflectance = layers.addImageryProvider(new Cesium.WebMapTileServic
     tilingScheme: gibs.GeographicTilingScheme()
 }));
 
-correctedReflectance.alpha = 1.0;    // 0.0 is transparent, 1.0 is opaque
-*/
+correctedReflectance.alpha = 0.0;    // 0.0 is transparent, 1.0 is opaque
 
-/*
 var night = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
     url: "//map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?layer=VIIRS_CityLights_2012",
     layer: "VIIRS_CityLights_2012",
@@ -30,8 +27,7 @@ var night = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvide
     tilingScheme: gibs.GeographicTilingScheme()
 }));
 
-night.alpha = 1.0;
-*/
+night.alpha = 0.0;
 
 var soilTemperature = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
     url: "//map1b.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2015-06-21&layer=SMAP_L4_Soil_Temperature_Layer_1",
@@ -45,6 +41,8 @@ var soilTemperature = layers.addImageryProvider(new Cesium.WebMapTileServiceImag
     tilingScheme: gibs.GeographicTilingScheme()
 }));
 
+soilTemperature.alpha = 1.0;
+
 var soilMoisture = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
     url: "//map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2015-06-27&layer=SMAP_L4_Analyzed_Root_Zone_Soil_Moisture",
     layer: "SMAP_L4_Analyzed_Root_Zone_Soil_Moisture",
@@ -57,6 +55,9 @@ var soilMoisture = layers.addImageryProvider(new Cesium.WebMapTileServiceImagery
     tilingScheme: gibs.GeographicTilingScheme()
 }));
 
+soilMoisture.alpha = 0.5;
+
+/*
 var terraSeaIce = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
     url: "//map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2016-03-27&layer=MODIS_Terra_Sea_Ice",
     layer: "MODIS_Terra_Sea_Ice",
@@ -68,6 +69,7 @@ var terraSeaIce = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryP
     tileHeight: 256,
     tilingScheme: gibs.GeographicTilingScheme()
 }));
+*/
 
 var aquaSeaIce = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
     url: "//map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2016-03-27&layer=MODIS_Aqua_Sea_Ice",
@@ -81,6 +83,8 @@ var aquaSeaIce = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryPr
     tilingScheme: gibs.GeographicTilingScheme()
 }));
 
+aquaSeaIce.alpha = 1.0;
+
 var terraSnowCover = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
     url: "//map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2014-03-30&layer=MODIS_Terra_Snow_Cover",
     layer: "MODIS_Terra_Snow_Cover",
@@ -93,6 +97,9 @@ var terraSnowCover = layers.addImageryProvider(new Cesium.WebMapTileServiceImage
     tilingScheme: gibs.GeographicTilingScheme()
 }));
 
+terraSnowCover.alpha = 1.0;
+
+/*
 var aquaSnowCover = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
     url: "//map1b.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2014-03-30&layer=MODIS_Aqua_Snow_Cover",
     layer: "MODIS_Aqua_Snow_Cover",
@@ -104,17 +111,7 @@ var aquaSnowCover = layers.addImageryProvider(new Cesium.WebMapTileServiceImager
     tileHeight: 256,
     tilingScheme: gibs.GeographicTilingScheme()
 }));
-
-//ERROR: Cross-Origin Resource Sharing (no Access-Control-Allow-Origin header)
-/*
-var glacierData = layers.addImageryProvider(new Cesium.WebMapServiceImageryProvider({
-    url: "//www.glims.org/mapservice?layer=glac_lines",
-}));
-
-glacierData.alpha = 0.5;
 */
-//http://www.glims.org/mapservice?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=wgi_points&CRS=EPSG%3A3857&STYLES=&WIDTH=1167&HEIGHT=1017&BBOX=-11023510.415200116%2C-4152293.6083030845%2C-5314581.646636872%2C822839.6887224675
-//https://map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2014-03-30&layer=MODIS_Aqua_Snow_Cover&tilematrixset=EPSG4326_500m&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&TileMatrix=0&TileCol=0&TileRow=0
 
 var coastlines = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
     url: "//map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?l=Reference_Features",
@@ -127,6 +124,8 @@ var coastlines = layers.addImageryProvider(new Cesium.WebMapTileServiceImageryPr
     tileHeight: 256,
     tilingScheme: gibs.GeographicTilingScheme()
 }));
+
+coastlines.alpha = 1.0;
 
 // Add terrian data source
 var terrainProvider = new Cesium.CesiumTerrainProvider({
